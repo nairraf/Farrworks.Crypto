7e09023c91e9a764c0b15c1c70199564c9fcfbee
ian.farr <ian.farr@farrworks.com>
2020-01-21 02:32:12 -0500

    working basic encrypt and decrypt using 3DES CBC. This shouldn't be used for highly sensitive data, but should be fine for less sensitive data requirements. a random IV is used every time, so encrypting the same string will always appear to be different. The IV is burried within the cipher text to facilitate decryption. TODO: better fallback than md5 (16byte)

---
+++ Farrworks.Crypto.Tests/Basic_TrippleDesProvider_Should.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using Farrworks.Crypto.Basic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Farrworks.Crypto.Tests
+{
+   
+    public class Basic_TrippleDesProvider_Should
+    {
+        [Test]
+        public void EncryptAndDecrypt()
+        {
+            System.Diagnostics.Debugger.Launch();
+
+            // generate a new random 192bit symetric key
+            TripleDESCryptoServiceProvider csp = new TripleDESCryptoServiceProvider();
+            csp.GenerateKey();
+            string key = Convert.ToBase64String(csp.Key);
+            
+            // create our TripleDesProvider objects with our new key
+            TripleDesProvider tdes1 = new TripleDesProvider(key);
+            TripleDesProvider tdes2 = new TripleDesProvider(key);
+
+            // test encryption and decription across tdes objects
+            string toEncrypt = "this is a test";
+            string cipherText = tdes1.BasicEncrypt(toEncrypt);
+            string decryptedText = tdes2.BasicDecrypt(cipherText);
+
+            Assert.That(decryptedText, Is.EqualTo(toEncrypt));
+        }
+
+        [Test]
+        public void NotFailOnLargeKey()
+        {
+            System.Diagnostics.Debugger.Launch();
+            Assert.DoesNotThrow(() =>
+            {
+                TripleDesProvider tdes1 = new TripleDesProvider("1q2w3e4r5t6y7u8i9o0p1q2w3e4r5t6y7u8i9o0pqawsedrftgyhujikolp");
+            });
+        }
+
+        [Test]
+        public void NotFailOnSmallKey()
+        {
+            System.Diagnostics.Debugger.Launch();
+            Assert.DoesNotThrow(() =>
+            {
+                TripleDesProvider tdes1 = new TripleDesProvider("a");
+            });
+        }
+    }
+}
---
+++ Farrworks.Crypto/Basic/TripleDesProvider.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Farrworks.Crypto.Basic
+{
+    public class TripleDesProvider : IDisposable
+    {
+        private readonly byte[] _keyBytes;
+        private TripleDESCryptoServiceProvider _tdesCrypto;
+
+        public TripleDesProvider(string key)
+        {
+            try 
+            {
+                // try and convert the key from a Base64String.
+                // this should work for any valid key produced via TripleDESCryptoServiceProvider.GenerateKey()
+                // which will produce a 24byte (192bit) symetric key
+                _keyBytes = Convert.FromBase64String(key);
+            }
+            catch
+            {
+                // fallback to md5 hash - which produces a 16byte hash (128bit symetric key)
+                // this may needed for keys that aren't formated properly.
+                // or fail to be imported properly for some reason.
+                MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                _keyBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+                md5.Dispose();
+            }
+            
+            _tdesCrypto = new TripleDESCryptoServiceProvider();
+            _tdesCrypto.Key = _keyBytes;
+            _tdesCrypto.Mode = CipherMode.CBC;
+        }
+
+        public string BasicEncrypt(string data)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            string ret = "";
+            try
+            {
+                string cipherText = Convert.ToBase64String(_tdesCrypto.CreateEncryptor().TransformFinalBlock(buffer, 0, buffer.Length));
+                string iv = Convert.ToBase64String(_tdesCrypto.IV);
+                // remove the = in the iv string, which is the last character
+                iv = iv.Substring(0, iv.Length - 1);
+                // bury the IV within the cipherText
+                char[] combinedCipher = new char[iv.Length + cipherText.Length];
+                int pointerIV = 0;
+                int pointerCT = 0;
+                for (int i=0; i<combinedCipher.Length; i++)
+                {
+                    if (i % 2 == 0 && pointerIV < iv.Length)
+                    {
+                        combinedCipher[i] = iv[pointerIV];
+                        pointerIV++;
+                    } 
+                    else
+                    {
+                        combinedCipher[i] = cipherText[pointerCT];
+                        pointerCT++;
+                    }
+                }
+                ret = new string(combinedCipher);
+            }
+            catch
+            {
+                // do nothing for now
+            }
+
+            return ret;
+        }
+
+        public string BasicDecrypt(string encryptedData)
+        {
+            //retrieve the IV that is burried within the cipherText
+            char[] arrIV = new char[11];
+            char[] arrCT = new char[24];
+            int pointerIV = 0;
+            int pointerCT = 0;
+
+            // we have 11 characters of IV burried within the even indexes. extract them and assign them to arrIV. 
+            // the rest is assigned to arrCT.
+            for (int i=0; i<encryptedData.Length; i++)
+            {
+                if (i % 2 == 0 && pointerIV < arrIV.Length)
+                {
+                    arrIV[pointerIV] = encryptedData[i];
+                    pointerIV++;
+                }
+                else
+                {
+                   arrCT[pointerCT] = encryptedData[i];
+                   pointerCT++;
+                }
+            }
+
+            string cipherText = new string(arrCT);
+            string iv = new string(arrIV);
+            // add back the trailing '=' sign for our IV
+            iv = iv + "=";
+            
+            // assign our IV
+            _tdesCrypto.IV = Convert.FromBase64String(iv);
+          
+            // get our buffer and decrypt.
+            byte[] buffer = Convert.FromBase64String(cipherText);
+            string ret = "";
+            try
+            {
+                ret = Encoding.UTF8.GetString(_tdesCrypto.CreateDecryptor().TransformFinalBlock(buffer, 0, buffer.Length));
+            }
+            catch
+            {
+                // do nothing for now
+            }
+            return ret;
+        }
+
+        public void Dispose()
+        {
+            _tdesCrypto.Dispose();
+        }
+    }
+}
